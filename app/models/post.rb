e9e24fd413b7b9e7d04fa9cc69189bcbab578dae
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-03-03 21:18:51 +0900

    編集・削除機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :comments, dependent: :destroy
+  has_many :comments
 
   validates :text, presence: true
 end
