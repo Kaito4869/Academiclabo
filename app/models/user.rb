24dbabcda32ba14e3c655ef95bfcb7364471a40e
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-03-04 18:32:21 +0900

    コメント機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :comments
+
+  with_options presence: true do
+    validates :nickname
+    validates :occupation
+    validates :name
+    validates :email
+    validates :password
+  end
 end
