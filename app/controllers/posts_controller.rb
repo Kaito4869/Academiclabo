c2eeb18bd5e74c371e0f337b9f0bc6670f2f615c
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-03-13 00:35:58 +0900

    viewの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).order('created_at DESC')
-    @tags = Tag.all
+    @tags = Tag.all.order('grade_id ASC')
   end
 
   def new
