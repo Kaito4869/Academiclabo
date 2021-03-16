85764b6bad29b210910ef712ecfe106ba7ea9b5f
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-03-16 15:06:54 +0900

    ビューの修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
     @tag = Tag.find(params[:post_id])
-    @posts = @tag.posts
+    @posts = @tag.posts.order('created_at DESC')
   end
 end
