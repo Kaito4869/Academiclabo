b51d98b1b9206efdda410f16c559433653d91102
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-03-19 19:28:56 +0900

    新規登録の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,15 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :grade
+
   with_options presence: true do
-    validates :nickname
-    validates :occupation
-    validates :name
+    validates :nickname, length: { maximum: 6 }
+    validates :grade_id, numericality: { other_than: 1 }
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
     validates :email
-    validates :password
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   end
 end
