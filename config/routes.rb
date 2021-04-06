037f6dd6fbac4edc6c7f608859582bc30e506a87
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-04-06 17:56:37 +0900

    アプリ紹介ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
+  resources :labos, only: [:new]
   resources :posts do
     resources :comments, only: [:create]
     resources :tags, only: [:index]
