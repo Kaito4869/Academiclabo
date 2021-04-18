b23d99a8f6d36a954793d46242fe25578c446f3d
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-04-18 13:19:39 +0900

    ページネーション機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:user).order('created_at DESC').limit(20)
+    @posts = Post.includes(:user).order('created_at DESC').page(params[:page]).per(10)
     @tags = Tag.all.order('subject_id ASC')
     @all_ranks = Post.find(Like.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
   end
